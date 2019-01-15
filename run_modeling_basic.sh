9d06a0c88e3e902d8cf13c2300ab610913f4d3cf
Rodolfo Dirack <rodolfo_profissional@hotmail.com>
2019-01-15 14:47:30 -0300

    Edição do script run_modeling_basic.sh e arquivo Makefile para compilação

--- run_modeling_basic.sh
+++ run_modeling_basic.sh
@@ -1,19 +1,92 @@
 #!/bin/bash
+#
+# run_modeling_basic.sh (Shell Script)
+#
+# Necessita do pacote Seismic Unix instalado para visualização
+#
+# Objetivo: Basic acoustic wave equation modeling.
+#
+# Versão 2.0 (Modificada)
+#
+# Original: Felipe Timóteo 08/01/2019
+#
+# Programador: Rodolfo A. C. Neves 14/01/2019
+# 
+# email: rodolfo_profissional@hotmail.com
+# 
+# Licença: Software de uso livre e código aberto.
 
-#compile modeling using gfortran
-gfortran modeling_basic.f90 -o run_modeling_basic
+#----------------{ Configuração de variáveis }-----------------------------#
 
-#run modeling basic
-./run_modeling_basic
+# Versão deste programa
+VERSAO="Versão 2.0"
 
+#--------------------------------------------------------------------------#
 
+MENSAGEM_USO="
+$(basename $0) [-h | -v | -c | -e | -r ]
+-h --help Exibe esta mensagem de ajuda e sai
+-v --version Exibe a Versão do programa e sai
+-c Compila os arquivos necessários deste programa em fortran 90
+-e Executar programa de modelagem
+-r Remover arquivos .bin 
+Exemplo de uso: 
+	bash$ $0 -c # Compilar arquivos .f90
+	bash$ $0 -e # Executar programa
+	bash$ $0 -r # Remover arquivos .bin
+"
+# Verificar se o usuário forneceu algum parâmetro
+[ -z "$1" ] && {
+	echo "O usuário não forneceu nenhum parâmetro!" 
+	echo "Digite $0 -h para obter ajuda!" 
+	exit 1 
+}
 
-#visualize results - it's necessary seismic unix
+case "$1" in
+-h | --help) ## Exibir ajuda
+	echo -e "$MENSAGEM_USO"
+	exit 0
+;;
 
-#Snapshots
-xmovie n1=301 n2=301 sleep=1 loop=1 < snapshots.bin
+-v | --version) ## Exibir versão
+	echo "$VERSAO"
+	exit 0
+;;
 
-#Seismogram
-ximage n1=1001 < seismogram.bin
+-c) ## Compilação
 
-#rm *.bin run_modeling_basic
+	# Compilação com arquivo Makefile
+	make 
+
+	exit 0
+
+;;
+
+-e) ## Rodar modeling_basic
+
+	./modeling_basic
+
+	# Visualizar (TODO:Basta descomentar)
+	
+	#Snapshots
+	#xmovie n1=301 n2=301 sleep=1 loop=1 < snapshots.bin
+
+	#Seismogram
+	#ximage n1=1001 < seismogram.bin
+
+	exit 0
+;;
+
+-r) ## Opção para remover arquivos .bin 
+
+	rm *.bin 
+	
+	exit 0
+
+;;
+*) ## Usuário forneceu parâmetro desconhecido
+	echo -e "\033[31mERRO: Opção $1 desconhecida!\033[m"
+	echo "\033[31mDigite $0 -h para obter ajuda\033[m"
+	exit 3
+;;
+esac
